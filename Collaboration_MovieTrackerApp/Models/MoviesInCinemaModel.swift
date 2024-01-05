a49b9b641c11a5f8267da45e742a9a9ebac21c53
Jamaტa <jamata94@gmail.com>
2024-01-06 01:10:17 +0400

    setup Genres
    add genres enum and shown in movie card

--- Collaboration_MovieTrackerApp/Models/MoviesInCinemaModel.swift
+++ Collaboration_MovieTrackerApp/Models/MoviesInCinemaModel.swift
@@ -28,7 +28,7 @@ struct DateRange: Codable {
 }
 
 struct Movie: Codable {
-    let genreIds: [Int]
+    let genreIds: [Genre]
     let id: Int
     let shortInfo: String
     let popularity: Double
@@ -46,5 +46,51 @@ struct Movie: Codable {
         case releaseDate = "release_date"
         case title
         case imdbRating = "vote_average"
+    }
+}
+
+enum Genre: Int, Codable {
+    case action = 28
+    case adventure = 12
+    case animation = 16
+    case comedy = 35
+    case crime = 80
+    case documentary = 99
+    case drama = 18
+    case family = 10751
+    case fantasy = 14
+    case history = 36
+    case horror = 27
+    case music = 10402
+    case mystery = 9648
+    case romance = 10749
+    case scienceFiction = 878
+    case tvMovie = 10770
+    case thriller = 53
+    case war = 10752
+    case western = 37
+    
+    var rawValue: String {
+        switch self {
+        case .action: "Action"
+        case .adventure: "Adventure"
+        case .animation: "Animation"
+        case .comedy: "Comedy"
+        case .crime: "Crime"
+        case .documentary: "Documentary"
+        case .drama: "Drama"
+        case .family: "Family"
+        case .fantasy: "Fantasy"
+        case .history: "History"
+        case .horror: "Horror"
+        case .music: "Music"
+        case .mystery: "Mystery"
+        case .romance: "Romance"
+        case .scienceFiction: "ScienceFiction"
+        case .tvMovie: "TvMovie"
+        case .thriller: "Thriller"
+        case .war: "War"
+        case .western: "Western"
+        }
     }
 }
--- Collaboration_MovieTrackerApp/Screens/MoviesInCinema/MainView/MoviesInCinemaView.swift
+++ Collaboration_MovieTrackerApp/Screens/MoviesInCinema/MainView/MoviesInCinemaView.swift
@@ -29,7 +29,7 @@ struct MoviesInCinemaView: View {
                         image: viewModel.generateImageURL(for: movie.posterPath)!,
                         title: movie.title,
                         shortInfo: movie.shortInfo,
-                        genre: "Genre"
+                        genre: movie.genreIds.first?.rawValue ?? "",
                     )
                 }
             }

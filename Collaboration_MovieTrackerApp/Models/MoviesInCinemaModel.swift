c276553029d046b274957b36d0de52a704ab7fbc
Jamaტa <jamata94@gmail.com>
2024-01-05 22:20:34 +0400

    Create MovieModel

--- Collaboration_MovieTrackerApp/Models/MoviesInCinemaModel.swift
+++ Collaboration_MovieTrackerApp/Models/MoviesInCinemaModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct MoviesData: Decodable {
+    let movies: [MovieResult]
+}
+
+// MARK: - Result
+struct MovieResult: Codable {
+    let genreIDS: [Int]
+    let id: Int
+    let overview: String
+    let popularity: Double
+    let posterPath, releaseDate, title: String
+    let imdbRating: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case genreIDS = "genre_ids"
+        case id
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case imdbRating = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
--- Collaboration_MovieTrackerApp/Screens/MoviesInCinema/MainView/MoviesInCinemaView.swift
+++ Collaboration_MovieTrackerApp/Screens/MoviesInCinema/MainView/MoviesInCinemaView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct MoviesInCinemaView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AppColor.background.ignoresSafeArea(.all)
+           
+            ScrollView {
+                VStack(spacing: 12) {
+                    ListRowView(image: "defaultImage", title: "Title", shortInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", genre: "test", imdb: "IMDB", imdbRating: "4.8")
+                    ListRowView(image: "defaultImage", title: "Title", shortInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", genre: "test", imdb: "IMDB", imdbRating: "4.8")
+                    ListRowView(image: "defaultImage", title: "Title", shortInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", genre: "test", imdb: "IMDB", imdbRating: "4.8")
+                    ListRowView(image: "defaultImage", title: "Title", shortInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", genre: "test", imdb: "IMDB", imdbRating: "4.8")
+                    ListRowView(image: "defaultImage", title: "Title", shortInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", genre: "test", imdb: "IMDB", imdbRating: "4.8")
+                    ListRowView(image: "defaultImage", title: "Title", shortInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ", genre: "test", imdb: "IMDB", imdbRating: "4.8")
+                }
+            }
+            
+            
+        }
     }
 }
 

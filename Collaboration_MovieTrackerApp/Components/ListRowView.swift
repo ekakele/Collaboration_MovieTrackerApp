11b8f614c43b4559de8d3605c45aa615b44120e3
ekakele <ekaakelenjeridze@gmail.com>
2024-01-05 16:26:51 +0400

    remove backgroundColor in ListRowView

--- Collaboration_MovieTrackerApp/Components/ListRowView.swift
+++ Collaboration_MovieTrackerApp/Components/ListRowView.swift
@@ -22,7 +22,6 @@ struct ListRowView: View {
             itemImageView
             fullInfoStackView
         }
-        .background(AppColor.background)
     }
     
     //MARK: Components
@@ -115,4 +114,5 @@ struct ListRowView: View {
 
 #Preview {
     ListRowView(image: "defaultImage", title: "Title", shortInfo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum", genre: "test", imdb: "IMDB", imdbRating: "4.8")
+        .background(AppColor.background)
 }

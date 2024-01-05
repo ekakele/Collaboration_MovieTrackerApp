ecaf20dbf221697176e8849b0927d12574427b7f
ekakele <ekaakelenjeridze@gmail.com>
2024-01-05 13:02:40 +0400

    Initial Commit

---
+++ Collaboration_MovieTrackerApp/Collaboration_MovieTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Collaboration_MovieTrackerAppApp.swift
+//  Collaboration_MovieTrackerApp
+//
+//  Created by Eka Kelenjeridze on 05.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Collaboration_MovieTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

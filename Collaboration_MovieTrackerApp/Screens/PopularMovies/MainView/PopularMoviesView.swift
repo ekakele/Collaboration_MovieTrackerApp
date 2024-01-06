66ad1831f0c8af4f4cce6c288ea9c16d705a7b3f
Mariam Joglidze <mariamjoglidze1@gmail.com>
2024-01-06 14:19:40 +0400

    create PopularMoviesView, make network call

--- Collaboration_MovieTrackerApp/Screens/PopularMovies/MainView/PopularMoviesView.swift
+++ Collaboration_MovieTrackerApp/Screens/PopularMovies/MainView/PopularMoviesView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct PopularMoviesView: View {
+    @StateObject var viewModel = PopularMoviesViewModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AppColor.background
+                .ignoresSafeArea()
+            
+            listView
+        }
+    }
+    
+    private var listView: some View {
+        List {
+            ForEach(viewModel.movies, id: \.id) { movie in
+                ListRowView(image: viewModel.generateImageURL(for: movie.image)!,
+                            title: movie.title,
+                            shortInfo: movie.shortInfo,
+                            genre: movie.genre.first?.rawValue ?? "",
+                            imdb: "IMDB",
+                            imdbRating: String(movie.imdbRating)
+                )
+            }
+            .listRowBackground(Color.clear)
+        }
+        .listStyle(PlainListStyle())
     }
 }
 

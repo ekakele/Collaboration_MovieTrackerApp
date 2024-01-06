fba243d74ea91e522f677abdd9008cc9d6dadbce
ekakele <ekaakelenjeridze@gmail.com>
2024-01-06 19:56:01 +0400

    integrate Sheet-based navigation for details page

--- Collaboration_MovieTrackerApp/Screens/PopularPersons/DetailsView/PopularPersonsDetailsView.swift
+++ Collaboration_MovieTrackerApp/Screens/PopularPersons/DetailsView/PopularPersonsDetailsView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct PopularPersonsDetailsView: View {
+    // MARK: - Properties
+    @StateObject var viewModel = PopularPersonsDetailsViewModel()
+//    @State var name = ""
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello world!")
+//            .onAppear(perform: {
+//                viewModel.fetchData(with: viewModel.person!.id)
+//            })
     }
 }
 
--- Collaboration_MovieTrackerApp/Screens/PopularPersons/MainView/PopularPersonsView.swift
+++ Collaboration_MovieTrackerApp/Screens/PopularPersons/MainView/PopularPersonsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct PopularPersonsView: View {
     // MARK: - Properties
     @StateObject var viewModel = PopularPersonsViewModel()
+    @State private var showDetailsPage: Bool = false
     
     // MARK: - Body
     var body: some View {
@@ -28,18 +29,34 @@ struct PopularPersonsView: View {
         .padding(.vertical, 5)
     }
     
+    private var detailsPageNavigatorView: some View {
+        VStack {
+            setupListView
+        }
+        .padding(.vertical, 5)
+    }
+    
+    
     private var setupListView: some View {
         List() {
             ForEach(viewModel.persons, id: \.id) { person in
-                ListRowView(
-                    image: viewModel.generateImageURL(for: person.profilePath)!,
-                    title: person.name,
-                    shortInfo: "Known for:  \(person.knownForDepartment)",
-                    genre: person.genderString,
-                    imdb: "Popularity:",
-                    imdbRating: "\(person.popularity)"
-                )
-                .listRowSeparator(.hidden)
+                Button(action: {
+                    showDetailsPage.toggle()
+                }, label: {
+                    ListRowView(
+                        image: viewModel.generateImageURL(for: person.profilePath)!,
+                        title: person.name,
+                        shortInfo: "Known for:  \(person.knownForDepartment)",
+                        genre: person.genderString,
+                        imdb: "Popularity:",
+                        imdbRating: "\(person.popularity)"
+                    )
+                    .listRowSeparator(.hidden)
+                })
+                .sheet(isPresented: $showDetailsPage) {
+                    PopularPersonsDetailsView()
+                        .presentationDetents([.fraction(0.99)])
+                }
             }
             .listRowBackground(Color.clear)
         }

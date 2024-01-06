e5e72a6d9a9150b16891df46a85444d220a47a6b
razmaa <145837511+razmaa@users.noreply.github.com>
2024-01-06 16:31:30 +0400

    Create Popular TV Series View

--- Collaboration_MovieTrackerApp/Screens/PopularTVSeries/MainView/PopularTVSeriesView.swift
+++ Collaboration_MovieTrackerApp/Screens/PopularTVSeries/MainView/PopularTVSeriesView.swift
@@ -6,10 +6,25 @@
 //
 
 import SwiftUI
+import GenericNetworkManager
 
 struct PopularTVSeriesView: View {
+    @StateObject var viewModel = PopularTVSeriesViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            List(viewModel.tvShows, id: \.id) { tvShow in
+                ListRowView(
+                    image: viewModel.generateImageURL(for: tvShow.posterPath) ?? "defaultImage" ,
+                    title: tvShow.name,
+                    shortInfo: tvShow.overview,
+                    genre: Genre.genreString(for: tvShow.genreIds.first ?? 0),
+                    imdb: "IMDB",
+                    imdbRating: String(tvShow.voteAverage)
+                )
+            }
+        }
+        .padding(.vertical, 5)
     }
 }
 

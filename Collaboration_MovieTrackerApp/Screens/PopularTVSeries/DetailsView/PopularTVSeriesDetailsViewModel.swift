d8052ee56e7db61b98ae750bb66ce6ebb9a930cf
razmaa <145837511+razmaa@users.noreply.github.com>
2024-01-07 13:24:54 +0400

    Setup Popular TV Series Details ViewModel

--- Collaboration_MovieTrackerApp/Screens/PopularTVSeries/DetailsView/PopularTVSeriesDetailsViewModel.swift
+++ Collaboration_MovieTrackerApp/Screens/PopularTVSeries/DetailsView/PopularTVSeriesDetailsViewModel.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import GenericNetworkManager
+
+class PopularTVSeriesDetailsViewModel: ObservableObject {
+    
+    private var networkManager = GenericNetworkManager(baseURL: "https://api.themoviedb.org")
+    @Published var showDetails: TVShowDetails?
+    
+    init() {
+        fetchDetails()
+    }
+    
+    func fetchDetails() {
+        
+        let id = Int()
+        let endpoint = "3/tv/\(id)?api_key=fc0c012be098a15452a64aa9c7fa252b"
+        
+        networkManager.fetchData(endpoint: endpoint) { (result: Result<TVShowDetails, Error>) in
+            switch result {
+            case .success(let response):
+                DispatchQueue.main.async {
+                    self.showDetails = response
+                }
+            case .failure(let error):
+                print("Failed to fetch TV shows: \(error)")
+            }
+        }
+    }
+    
+    
+    func  generateImageURL(for profilePath: String?) -> String {
+        guard let profilePath = profilePath else { return "" }
+        
+        let baseImageURL = "https://image.tmdb.org/t/p/"
+        let imageSize = "original"
+        
+        return baseImageURL + imageSize + profilePath
+    }
+    
+}
+
